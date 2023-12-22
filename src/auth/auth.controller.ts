025329e12d906e8e57a739173587acbd51182f57
ababacar cisse <ababacarcisse18@gmail.com>
2023-12-22 17:10:43 +0100

    hashpassword

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,9 +1,11 @@
 import { Body, Controller, Post } from '@nestjs/common';
-
+import { AuthService } from './auth.service';
+export type AuthBody= {email:string;password:string}
 @Controller('auth')
 export class AuthController {
+constructor(private readonly authService:AuthService ){}
     @Post('login')
-    async login(@Body() authBody: {email:string;password:string}){
+    async login(@Body() authBody:AuthBody){
         console.log(authBody);
         return ''; 
     }
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,6 +1,34 @@
 import { Body, Injectable, Post } from '@nestjs/common';
+import { AuthBody } from './auth.controller';
+import { PrismaService } from 'src/prisma.service';
+import bcrypt from 'bcrypt'
 
 @Injectable()
 export class AuthService {
-   
+    constructor(private readonly prisma:PrismaService){}
+   async loin({authBody}:{authBody:AuthBody }){
+    const{email,password}=authBody;
+    const hashPassword=await this.hashPassword({password});
+    console.log(hashPassword);
+
+    //je vérifie si l'user existe 
+    const existingUser=await this.prisma.user.findUnique({
+        where:{
+            email:authBody.email
+        },
+    });
+    if(!existingUser){
+        throw new Error("l'utilisateur n'existe pas ")
+    }
+    const isPasswordSame= password === existingUser.password;
+    if(!isPasswordSame){
+        throw new Error("le mot de pass est invalide ");
+    }
+    return existingUser;  
+   }
+  async hashPassword({password}: {password:string}){
+    const hashedPassword=await bcrypt.hash(password,10)
+    return hashedPassword;
+   }
+
 }
